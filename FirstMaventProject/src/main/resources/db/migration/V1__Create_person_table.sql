f8afe14d7f8a04dbddcdb103ffa789e6eec28c01
Abdesselam DOUICHI <abdoudouichi@MacBook-Pro-de-Abdesselam.local>
2016-10-09 19:56:33 +0200

    add rows

--- FirstMaventProject/src/main/resources/db/migration/V1__Create_person_table.sql
+++ FirstMaventProject/src/main/resources/db/migration/V1__Create_person_table.sql
@@ -2,6 +2,3 @@ create table PERSON (
     ID int not null,
     NAME varchar(100) not null
 );
-insert into PERSON (ID, NAME) values (1, 'Axel');
-insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
-insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
